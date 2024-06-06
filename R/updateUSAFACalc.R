b0767753230eb3294cf5c95d5e38576580e865f8
Eichholz <joseph.eichholz@afacademy.af.edu>
2024-06-06 11:23:00 -0600

    Improve updateUSAFACalc to allow more flexibility for possible emergency situations.

--- R/updateUSAFACalc.R
+++ R/updateUSAFACalc.R
@@ -1,6 +1,9 @@
 #' Updates the USAFACalc package, if update is available, from github. Does not
 #' seek to update dependencies.
+#' @param repo the repo to install from. Defaults to proper value.
+#' @inheritParams devtools::install_github
+#' @param ... will be passed into devtools::install_github
 #' @export
-updateUSAFACalc = function(){
-  devtools::install_github("jeichholz/USAFACalc",upgrade="never")
+updateUSAFACalc = function(repo="jeichholz/USAFACalc",upgrade="never",...){
+  devtools::install_github(repo,upgrade=upgrade,...)
 }
