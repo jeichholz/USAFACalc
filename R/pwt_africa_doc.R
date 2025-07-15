28fdeecc42ef14dc2d66a2b911fae6a75993e48a
Eichholz <joseph.eichholz@afacademy.af.edu>
2025-07-15 13:15:09 -0600

    Add pwt_africa, pwt_asia, and pwt_euro for project 1.

---
+++ R/pwt_africa_doc.R
@@ -0,0 +1,15 @@
+#' GDP Data for Africa
+#'
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{country}{Country Name}
+#'   \item{isocode}{ISO 3166-1 alpha-3 country code.}
+#'   \item{year}{Year}
+#'   \item{rgdpo}{Output-side real GDP at chained PPPs (in million 2017 USD)}
+#'   \item{pop}{Population (in millions)}
+#'   \item{rnna}{Capital stock at constant 2017 national prices (in million 2017 USD)}
+#'   \item{region}{region the country is in}
+#'   ...
+#' }
+"pwt_africa"
