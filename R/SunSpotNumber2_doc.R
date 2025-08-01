5118f5262ba43d62d05494ef1899051daae5d17e
Eichholz <joseph.eichholz@afacademy.af.edu>
2025-08-01 11:38:47 -0600

    Add MedianHousePrice, USFoodExpenditure, and SunSpotNumber2

---
+++ R/SunSpotNumber2_doc.R
@@ -0,0 +1,16 @@
+#' Sun Spot Data
+#'@description
+#' A dataset containing the Sunspot Number (calculated by physicists) as a function of the year from 1975 to 2000.
+#' The sunspot number is a measure of the level of sunspot activity on the Sun’s surface. Sunspots are areas of intense magnetic activity…they are associated with the Sun’s magnetic field and can impact communication systems including satellite communication and radio broadcasts, especially during times where the sunspot number is highest.
+#'
+#'@details
+#'A dataset containing the Sunspot Number (calculated by physicists) as a function of the year from 1975 to 2000.
+#'
+#'
+#' @format A data frame with 26 observations and 2 variables:
+#' \describe{
+#'   \item{Year}{The year of observation}
+#'   \item{Number}{The sunspot number calculated.}
+#'   ...
+#' }
+"SunSpotNumber2"
---
+++ R/USFoodExpenditure_doc.R
@@ -0,0 +1,14 @@
+#' US Food Expenditures
+#'@description
+#'  A dataset containing total food expenditure of American families as a percentage of disposable personal income as a function of the year from 1950 to 2010.
+#'
+#'
+#'@format A data frame with 61 observations and 2 variables:
+#' \describe{
+#'   \item{Year}{The year of observation}
+#'   \item{Percent}{Percent of disposable income spend on food.}
+#'   ...
+#'}
+#'@source https://humanprogress.org/. Accessed on July 15, 2025.
+#'
+"USFoodExpenditure"
