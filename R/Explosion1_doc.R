4604e818183a8f6faba7298ea5fa25fb9ee50055
Eichholz <joseph.eichholz@afacademy.af.edu>
2025-08-14 11:36:06 -0600

    Add T53 data and blast radius data.

---
+++ R/Explosion1_doc.R
@@ -0,0 +1,16 @@
+#' Blast data for nuclear explosion test in White Sands, New Mexico
+#'
+#'@description
+#'A modified dataset containing the blast radius (in meters) as a function of time (in seconds) for a nuclear test conducted by the United States in White Sands, New Mexico in 1945.
+#'
+#'
+#' @format A data frame with 23 observations and 2 variables:
+#' \describe{
+#'   \item{Time}{in seconds}
+#'   \item{Radius}{radius of blast in meters}
+#' }
+#'
+#' @source
+#' Taylor, G. “The Formation of a Blast Wave by a Very Intense Explosion. II. The Atomic Explosion of 1945.” Proceedings of the Royal Society A: Mathematical, Physical and Engineering Sciences, 1950, 201: 175–86.
+#'
+"Explosion1"
---
+++ R/TurboNormalizerT53_doc.R
@@ -0,0 +1,12 @@
+#' Data for a T53 with a turbonormalizer
+#'
+#'@description
+#'Contains fictional, but realistic data on the climb performance of a T-53 aircraft outfitted with a turbonormalizing system.
+#'
+#'
+#' @format A data frame with 13 observations and 2 variables:
+#' \describe{
+#'   \item{Time}{in seconds}
+#'   \item{Altitude}{in feet above the ground.}
+#' }
+"TurboNormalizer_T53"
